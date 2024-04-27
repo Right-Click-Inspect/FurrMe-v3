52ad68bd1130070b7000b2803c2dfff7014ebcbb
Lloren <storageaccntone@gmail.com>
2024-04-27 18:43:51 +0800

    Navbar Login Button Hide > Accout Name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     return (
         <Router>
             {/* <Signup /> */}
-            <PetPrev />
+            <Signup />
             {/* <Homepage/> */}
         </Router>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { FiChevronRight } from "react-icons/fi";
 import Logo from "../assets/FurrMe Logo.png";
+import DisplayPhoto from "../assets/pet photos/cat photos/cat_img1.jpg";
 import "../components/Navbar.css";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+    const closeDropdown = () => setDropdownOpen(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    useEffect(() => {
+        const closeMenuOnOutsideClick = (e) => {
+            if (
+                dropdownOpen &&
+                !document.querySelector(".iconDropDown").contains(e.target) &&
+                !document.querySelector(".accountName").contains(e.target)
+            ) {
+                setDropdownOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", closeMenuOnOutsideClick);
+
+        return () => {
+            document.removeEventListener("mousedown", closeMenuOnOutsideClick);
+        };
+    }, [dropdownOpen]);
 
     return (
         <div>
@@ -70,6 +96,31 @@ function Navbar() {
                 <Link to="/">
                     <button className="btn-login">Login</button>
                 </Link>
+                <h3 className="accountName" onClick={toggleDropdown}>
+                    Account Name
+                </h3>
+                <div className={`iconDropDown ${dropdownOpen ? "open" : ""}`}>
+                    <Link onClick={closeMobileMenu}>
+                        <h3 className="profileName" onClick={closeDropdown}>
+                            Frankie Richards
+                        </h3>
+                    </Link>
+                    <hr />
+                    <div className="dpNavList">
+                        <Link onClick={closeDropdown}>
+                            <h3>Adoption Requests</h3>
+                            <FiChevronRight className="rightIcon" />
+                        </Link>
+                        <Link onClick={closeDropdown}>
+                            <h3>Help & Support</h3>
+                            <FiChevronRight className="rightIcon" />
+                        </Link>
+                        <Link onClick={closeDropdown}>
+                            <h3>Logout</h3>
+                            <FiChevronRight className="rightIcon" />
+                        </Link>
+                    </div>
+                </div>
             </nav>
         </div>
     );
